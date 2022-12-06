6777c9e6bff5fffcea9f29953ade28b3c521e462
irinakolbun <kolbunirina01@gmail.com>
2022-12-05 18:06:48 -0800

    Added camera controls for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 5.0f;
     public float jumpForce = 10.0f;
-    public float gravity = 20.0f;
+    public float gravity = 9.81f;
 
     private CharacterController characterController;
+    public Camera camera;
     private Vector3 moveDirection = Vector3.zero;
 
     // Start is called before the first frame update
@@ -39,5 +40,27 @@ public class PlayerController : MonoBehaviour
 
         // Move the character
         characterController.Move(moveDirection * Time.deltaTime);
+        this.LookRotation(transform, camera.transform);
     }
+
+    public void LookRotation(Transform character, Transform camera)     
+    {
+        float yRot = Input.GetAxis("Mouse X") * 2f;
+        float xRot = Input.GetAxis("Mouse Y") * 2f;
+        character.localRotation *= Quaternion.Euler(0f, yRot, 0f);
+        camera.localRotation *= Quaternion.Euler(-xRot, 0f, 0f);
+        camera.localRotation = ClampRotationAroundXAxis(camera.localRotation);
+    }     
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        angleX = Mathf.Clamp(angleX, -90f, 90f);
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+        return q;
+    }    
 }
