77f3613e5f805e335e1a0bb58ef8d64dddce81f5
irinakolbun <kolbunirina01@gmail.com>
2022-12-05 17:24:55 -0800

    Added floor and logic for the player to move and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float jumpForce = 10.0f;
+    public float gravity = 20.0f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (characterController.isGrounded)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            moveDirection = (transform.forward * v) + (transform.right * h);
+            moveDirection = moveDirection.normalized * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the character
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
